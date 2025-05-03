80dbbc8283aca5fdeae0142ef4c5bd9b2f4af238
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-05-03 09:26:48 +0530

    selection sort

---
+++ src/Selectionsort.java
@@ -0,0 +1,30 @@
+public class Selectionsort {
+    public static void main(String[] args){
+        int arr[] = {2, 6, 3, 9, 1};
+        ssort(arr);
+    }
+
+    public static void ssort(int[] arr){
+        int size = arr.length;
+
+        for(int i = 0; i < size - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < size; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, minIndex, i); // Place smallest at front
+        }
+
+        for(int n : arr){
+            System.out.print(n + " ");
+        }
+    }
+
+    public static void swap(int[] arr , int a, int b){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+}
