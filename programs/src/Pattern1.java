6bc240a47b0b1b2b96d26e1ec143aadfcbfe1067
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-05-07 17:23:36 +0530

    comment-pattern 1

--- programs/src/Pattern1.java
+++ programs/src/Pattern1.java
@@ -1,3 +1,9 @@
+//Enter the n value : 8
+//        1,
+//        2,3,
+//        4,5,6,
+//        7,8.
+
 import java.util.Scanner;
 public class Pattern1 {
     public static void main(String[] args)
--- src/Cyclesort.java
+++ src/Cyclesort.java
@@ -1,4 +1,33 @@
-package PACKAGE_NAME;
+
+
+
 
 public class Cyclesort {
+
+    public static void main(String[] args)
+    {
+        int[] arr = {4,1,3,2};
+        csort(arr);
+        for(int i =0; i< arr.length ; i++){
+            System.out.print(arr[i]);
+        }
+    }
+    public static void csort(int[] arr)
+    {
+        int i=0;
+        while (i<arr.length)
+        {
+            int correctindex = arr[i] - 1;
+            if(arr[i] != arr[correctindex])  swap(arr, i, correctindex);
+            else i++;
+        }
+    }
+    public static void swap(int[] arr, int a, int b)
+    {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
 }
+
+
