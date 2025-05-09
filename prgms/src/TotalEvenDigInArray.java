ffc6a96a9c47f05c49827c35ff6f86e7f2244938
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-05-09 08:18:01 +0530

    total even digits in 2d array

---
+++ prgms/src/TotalEvenDigInArray.java
@@ -0,0 +1,25 @@
+public class TotalEvenDigInArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 333, 442332,234,55};
+        totalEvenDigits(arr);
+    }
+
+    private static void totalEvenDigits(int[] arr)
+    {
+        if(arr.length==0){
+            System.out.println("no elements in array");
+        }
+        int evenCount = 0;
+        for(int val : arr)
+        {
+            int digcount = 0;
+            while(val>0)
+            {
+                val=val/10;
+                digcount++;
+            }
+            if(digcount % 2==0) evenCount++ ;
+        }
+        System.out.println("There are " + evenCount + " evenDigits in this array");
+    }
+}
