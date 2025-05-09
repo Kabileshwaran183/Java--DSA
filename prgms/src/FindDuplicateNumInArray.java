6ab2096532cbadfa077b442d0d317b0f053d130d
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-05-09 11:44:27 +0530

    find duplicate number in an array - return 1st occ of duplicate value

---
+++ prgms/src/FindDuplicateNumInArray.java
@@ -0,0 +1,29 @@
+public class FindDuplicateNumInArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 5, 4, 3, 2};
+        int res = DuplicateNumber(arr);
+        System.out.print(res);
+    }
+    private static int DuplicateNumber(int[] arr)
+    {
+        int i= 0;
+        while (i<arr.length)
+        {
+            int correctindex = arr[i]-1;
+            if (i!=correctindex)
+            {
+                if(arr[i] == arr[correctindex]) return arr[i];
+                else swap(arr,i,correctindex);
+            }
+            else i++;
+        }
+        return arr.length;
+    }
+
+    private static void swap(int[] arr, int a, int b)
+    {
+        int temp = arr[a];
+        arr[a]=arr[b];
+        arr[b]= temp;
+    }
+}
